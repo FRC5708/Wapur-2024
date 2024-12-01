ec50650423f7510a9c6b184ba9d04f721a331395
Lincoln <lincoln@vulpes.com>
2024-12-01 14:01:23 -0500

    Added Drive System
    It kinda work
    Also auton(Kinda work)

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,90 +1,163 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
-import edu.wpi.first.wpilibj.XboxController;
 
 import java.util.Map;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
-import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.wpilibj.XboxController;
+
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.networktables.GenericEntry;
 
-/**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
- */
 public class Robot extends TimedRobot {
+  // The football robot drivetrain is a modified mecanum drive with
+  // 4 independent gearboxes.
 
   private final XboxController m_stick = new XboxController(0);
   
-  // Robot Ports
-  private final VictorSPX m_upperLeft = new VictorSPX(1);
-  private final VictorSPX m_lowerRight = new VictorSPX(2);
-  private final VictorSPX m_upperRight = new VictorSPX(3);
-  private final VictorSPX m_lowerLeft = new VictorSPX(4);
-  private GenericEntry m_drivePower;
+  private final VictorSPX m_frontLeftMotor = new VictorSPX(1);
+  private final VictorSPX m_frontRightMotor = new VictorSPX(4); 
+  private final VictorSPX m_backLeftMotor = new VictorSPX(3);
+  private final VictorSPX m_backRightMotor = new VictorSPX(2);
+  private GenericEntry m_autonSelect;
+  private long m_startTime;
 
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
-  @Override
-  public void robotInit() {
-  m_upperLeft.set(ControlMode.PercentOutput,0);
-  m_upperRight.set(ControlMode.PercentOutput,0);
-  m_lowerLeft.set(ControlMode.PercentOutput,0);
-  m_lowerRight.set(ControlMode.PercentOutput,0);
 
-  m_drivePower = Shuffleboard.getTab("Drive Variables")
-        .add("Power Slider", 1)
-        .withWidget(BuiltInWidgets.kNumberSlider)
-        .withProperties(Map.of("min", 0, "max", 1))
+
+  @Override //
+     public void robotInit() {
+    m_frontLeftMotor.set(ControlMode.PercentOutput, 0);
+    m_frontRightMotor.set(ControlMode.PercentOutput, 0);
+    m_backLeftMotor.set(ControlMode.PercentOutput, 0);
+    m_backRightMotor.set(ControlMode.PercentOutput, 0);
+
+    m_autonSelect = Shuffleboard.getTab("Auton Select")
+        .add("Auton Choose", 0)
         .getEntry();
   }
-
-  /**
-   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
-   * that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
-   */
-  
-
-  /** This function is called periodically during operator control. */
-  @Override
-  public void teleopPeriodic() {
-    double power = Math.pow(m_drivePower.getDouble(1.0),3);
-    if (m_stick.getAButton()){
-      m_upperLeft.set(ControlMode.PercentOutput,power);
-    }
-    else if (m_stick.getBButton()){
-      m_lowerLeft.set(ControlMode.PercentOutput,power);
-    }
-    else if (m_stick.getXButton()){
-      m_upperRight.set(ControlMode.PercentOutput,power);
-    }
-    else if (m_stick.getYButton()){
-      m_lowerRight.set(ControlMode.PercentOutput,power);
-    }
-    else {
-    m_upperLeft.set(ControlMode.PercentOutput,-Math.pow(m_stick.getLeftY(),3));
-    m_lowerLeft.set(ControlMode.PercentOutput,-Math.pow(m_stick.getLeftY(),3));
-    m_upperRight.set(ControlMode.PercentOutput,Math.pow(m_stick.getRightY(),3));
-    m_lowerRight.set(ControlMode.PercentOutput,Math.pow(m_stick.getRightY(),3));
-    }
+  @Override //Setting the current System Time
+  public void autonomousInit(){
+    m_startTime = System.currentTimeMillis();
   }
- 
+
+  @Override //Moving and other command shit
+  public void autonomousPeriodic() {
+      int auton = (int) m_autonSelect.getInteger(1);
+      if (auton == 1)
+      {
+          autonNineCrateFowardLeft();
+      }
+      else if (auton == 2)
+      {
+          autonNineCrateSidewaysLeft();
+      }
+       else if (auton == 3)
+      {
+          autonNineCrateFowardRight();
+      }
+        else if (auton == 4)
+      {
+          autonNineCrateSidewaysRight();
+      }
+      else {
+      mecanumDrive_Cartesian(0, 0, 0);
+      }
+  }
+
+  @Override //Safety so it should double check speed is set to 0
+  public void teleopInit() {
+    mecanumDrive_Cartesian(0, 0, 0);
+  }
+
+  @Override //Driving
+  public void teleopPeriodic() {
+  mecanumDrive_Cartesian(Math.pow(m_stick.getLeftX(), 3), Math.pow(m_stick.getLeftY(), 3), Math.pow(m_stick.getRightX(), 3));
+  }
+
+public void mecanumDrive_Cartesian(double x, double y, double rotation)
+{
+    double wheelSpeeds[] = new double[32];
+
+  //x, y, and rotation
+    wheelSpeeds[1] = -x + y + -rotation;
+    wheelSpeeds[2] = x + -y + -rotation;
+    wheelSpeeds[3] = x + y + -rotation;
+    wheelSpeeds[4] = -x + -y + -rotation;
+
+
+   normalize(wheelSpeeds);
+
+    m_frontLeftMotor.set(ControlMode.PercentOutput, wheelSpeeds[1]);
+    m_frontRightMotor.set(ControlMode.PercentOutput, wheelSpeeds[4]);
+    m_backLeftMotor.set(ControlMode.PercentOutput, wheelSpeeds[3]);
+    m_backRightMotor.set(ControlMode.PercentOutput, wheelSpeeds[2]);
+}   //mecanumDrive_Cartesian
+
+private void normalize(double[] wheelSpeeds)
+{
+    double maxMagnitude = Math.abs(wheelSpeeds[0]);
+
+    for (int i = 1; i < wheelSpeeds.length; i++)
+    {
+        double magnitude = Math.abs(wheelSpeeds[i]);
+
+        if (magnitude > maxMagnitude)
+        {
+            maxMagnitude = magnitude;
+        }
+    }
+
+    if (maxMagnitude > 1.0)
+    {
+        for (int i = 0; i < wheelSpeeds.length; i++)
+        {
+            wheelSpeeds[i] /= maxMagnitude;
+        }
+    }
+}
+    
+
+    public void autonNineCrateSidewaysLeft()
+    {
+     if(System.currentTimeMillis() - m_startTime < 4000) {
+          mecanumDrive_Cartesian(-1, -.1, 0);
+      }
+      else {
+        mecanumDrive_Cartesian(0,0, 0);
+      }
+    }
+
+    public void autonNineCrateFowardLeft()
+    {
+      if(System.currentTimeMillis() - m_startTime < 4000) {
+          mecanumDrive_Cartesian(-0.2, -1, 0);
+      }
+      else {
+        mecanumDrive_Cartesian(0,0, 0);
+      }
+    }
+    public void autonNineCrateSidewaysRight()
+    {
+     if(System.currentTimeMillis() - m_startTime < 4000) {
+          mecanumDrive_Cartesian(1, -.1, 0);
+      }
+      else {
+        mecanumDrive_Cartesian(0,0, 0);
+      }
+    }
+
+    public void autonNineCrateFowardRight()
+    {
+      if(System.currentTimeMillis() - m_startTime < 4000) {
+          mecanumDrive_Cartesian(0.1, -1, 0);
+      }
+      else {
+        mecanumDrive_Cartesian(0,0, 0);
+      }
+    }
+
 }
